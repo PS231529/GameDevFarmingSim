2939d2923761c768115893ac8117aaedd4edc210
PS231529 <92236089+PS231529@users.noreply.github.com>
2024-05-17 03:11:29 +0200

    Plowing and Seeding
    ur mom

--- Farming Sim/Assets/Scripts/ToolsCharacterController.cs
+++ Farming Sim/Assets/Scripts/ToolsCharacterController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class ToolsCharacterController : MonoBehaviour
 {
@@ -8,6 +9,14 @@ public class ToolsCharacterController : MonoBehaviour
     Rigidbody2D rgbd2d;
     [SerializeField] float offsetDistance = 1f;
     [SerializeField] float sizeOfInteractableArea = 1f;
+    [SerializeField] MarkerManager markerManager;
+    [SerializeField] TileMapReaderController tileMapReaderController;
+    [SerializeField] float maxDistance = 1.5f;
+    [SerializeField] CropsManager cropsManager;
+    [SerializeField] TileData plowableTiles;
+
+    Vector3Int selectedTilePosition;
+    bool selectable;
 
     private void Awake()
     {
@@ -17,13 +26,38 @@ public class ToolsCharacterController : MonoBehaviour
 
     private void Update()
     {
+        SelectTile();
+        CanSelectCheck();
+        Marker();
         if (Input.GetMouseButtonDown(0))
         {
-            UseTool();
+            if(UseToolWorld() == true)
+            {
+                return;
+            }
+            UseToolGrid();
         }
     }
 
-    private void UseTool()
+    private void SelectTile()
+    {
+        selectedTilePosition = tileMapReaderController.GetGridPosition(Input.mousePosition, true);
+    }
+
+    void CanSelectCheck()
+    {
+        Vector2 characterPosition = transform.position;
+        Vector2 cameraPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        selectable = Vector2.Distance(characterPosition, cameraPosition) < maxDistance;
+        markerManager.Show(selectable);
+    }
+
+    private void Marker()
+    {
+        markerManager.markedCellPosition = selectedTilePosition;
+    }
+
+    private bool UseToolWorld()
     {
         Vector2 position = rgbd2d.position + character.lastMotionVector * offsetDistance;
 
@@ -35,8 +69,29 @@ public class ToolsCharacterController : MonoBehaviour
             if (hit != null)
             {
                 hit.Hit();
-                break;
+                return true;
             }
+        }
+
+        return false;
+    }
+
+    private void UseToolGrid()
+    {
+        if(selectable == true)
+        {
+            TileBase tileBase = tileMapReaderController.GetTileBase(selectedTilePosition);
+            TileData tileData = tileMapReaderController.GetTileData(tileBase);
+            if(tileData != plowableTiles) { return; }
+            if (cropsManager.Check(selectedTilePosition))
+            {
+                cropsManager.Seed(selectedTilePosition);
+            }
+            else
+            {
+                cropsManager.Plow(selectedTilePosition);
+            }
+            
         }
     }
 }
